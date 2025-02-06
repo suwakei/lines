383fd786c14ec133db93c337dfc1c8e4edf80b05
keitosuwahara <keitosuwahara0816@gmail.com>
2025-02-06 20:34:39 +0900

    added version flag

--- cmd/steps.go
+++ cmd/steps.go
@@ -2,22 +2,45 @@ package cmd
 
 import (
 	"os"
+	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
+const VERSION string = "0.1.0"
 
+var (
+	path string
+	rootCmd = &cobra.Command{
+		Use:   "steps [PATH] [OPTIONS]",
+		Short: "steps counts the number of lines of dir or file that you input.",
+		Long: `steps counts the number of lines of dir or file that you input.
+		also this app can output result in [json, jsonc, yml, toml, txt] format
+		and also can ignore only particular file of extension with input your .gitignore file.
+		and so on ... please see flags help for more information.
+		`,
+		Args: cobra.MaximumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if version, _ := cmd.Flags().GetBool("version"); version {
+				fmt.Println(VERSION)
+				return 
+		}
 
-var rootCmd = &cobra.Command{
-	Use:   "steps [path]",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+		if len(args) > 0 {
+			if args[0] == "." {
+				path, _ = os.Getwd()
+			} else {
+				path = args[0]
+			}
+		} else if len(args) == 0 {
+			fmt.Println("[ERROR]: no path input!")
+			return
+		}
+		fmt.Println(path)
+	},
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 }
+)
 
 func Execute() {
 	err := rootCmd.Execute()
@@ -27,7 +50,8 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("version", "v", false, "Print version of this app")
+	
 }
 
 
