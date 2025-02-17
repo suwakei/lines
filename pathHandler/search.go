39af075bd38f20779be539b2ed85b807777bc54b
諏訪原慶斗 <131525647+suwakei@users.noreply.github.com>
2025-02-17 14:03:01 +0900

    search.go の更新

--- pathHandler/search.go
+++ pathHandler/search.go
@@ -8,12 +8,13 @@ import (
 
 func Search(path string, ignores map[string][]string) ([]string, error) {
 	var files []string
-	path, err := Parse(path)
+
+	parsedPath, err := Parse(path)
 	if err != nil {
 		return nil, err
 	}
 
-	fp.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+	err = fp.WalkDir(parsedPath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -21,22 +22,25 @@ func Search(path string, ignores map[string][]string) ([]string, error) {
 		if d.IsDir() {
 			if contains(ignores["dir"], path) {
 				return fp.SkipDir
-			} else {
-				return nil
 			}
+			return nil
 		}
 
-		if !d.IsDir() && contains(ignores["file"], path) {
+		if contains(ignores["file"], path) {
 			return nil
 		}
 
 		files = append(files, path)
 		return nil
 	})
+
+	if err != nil {
+		return nil, err
+	}
+
 	return files, nil
 }
 
 func contains(ignores []string, path string) bool {
 	return slices.Contains(ignores, fp.Base(path))
 }
-
