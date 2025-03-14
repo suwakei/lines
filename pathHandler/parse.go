e86ad05b71755bc9587c71a97a5a796e95a686bc
keitosuwahara <keitosuwahara0816@gmail.com>
2025-03-15 06:27:43 +0900

    removed Exist func

--- pathHandler/parse.go
+++ pathHandler/parse.go
@@ -1,8 +1,6 @@
 package pathHandler
 
 import (
-	"fmt"
-	"os"
 	fp "path/filepath"
 )
 
@@ -11,13 +9,5 @@ func Parse(path string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	if !Exists(absPath) {
-		return "", fmt.Errorf("[INFO]: path does not exist \" %s \"", absPath)
-	}
 	return fp.Clean(absPath), nil
-}
-
-func Exists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil
 }
